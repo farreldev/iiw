175cc561c07f3be8e0de816fbdc9ceb99506ca3f
farreldev <rickyalext@gmail.com>
2018-07-09 23:27:06 +0700

    edit index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let app = express();
 
 app.use(express.static('dist'));
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
    console.log('====================================');
    console.log('Express server is run on port 3000');
    console.log('====================================');
